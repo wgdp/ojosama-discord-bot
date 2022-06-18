620fa1414d42fd2a87e20ee514876ea6ccc56e38
wgdp <tatsuyori.shiraga@triple-four.com>
2022-06-18 23:19:37 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module ojosama-discord-bot
+
+go 1.18
+
+require (
+	github.com/bwmarrin/discordgo v0.25.0
+	github.com/jiro4989/ojosama v0.1.2
+)
+
+require (
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/ikawaha/kagome-dict v1.0.4 // indirect
+	github.com/ikawaha/kagome-dict/ipa v1.0.4 // indirect
+	github.com/ikawaha/kagome/v2 v2.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/jiro4989/ojosama"
+)
+
+func GetHelp() string {
+	f, err := os.Open("help.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+
+	text, err := ojosama.Convert(string(b), nil)
+	if err != nil {
+		panic(err)
+	}
+
+	return text
+}
+
+func onMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	splitedContent := strings.Split(m.Content, " ")
+
+	if splitedContent[0] != "/ojosama" {
+		return
+	}
+
+	if len(splitedContent) != 2 {
+		warningText, err := ojosama.Convert("コマンドが不正です！\n以下を参考にしてください！\n\n/ojosama 変換したいテキスト\n/ojosama --help\n\n", nil)
+		if err != nil {
+			panic(err)
+		}
+		s.ChannelMessageSend(m.ChannelID, warningText)
+		return
+	}
+	if splitedContent[0] == "/ojosama" {
+		if splitedContent[1] == "--help" {
+			s.ChannelMessageSend(m.ChannelID, GetHelp())
+			return
+		}
+		text, err := ojosama.Convert(splitedContent[1], nil)
+		if err != nil {
+			panic(err)
+		}
+
+		s.ChannelMessageSend(m.ChannelID, text)
+	}
+	return
+
+}
+
+func OjosamaErrorHandling(message string, err error) {
+	errText, err := ojosama.Convert(message + "\n以下エラーメッセージです！", nil)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(errText)
+	fmt.Println(err)
+}
+
+func main() {
+	discord, err := discordgo.New("Bot " + os.Getenv("CLIENT_TOKEN"))
+	if err != nil {
+		OjosamaErrorHandling("セッションの作成に失敗しました！", err)
+		return
+	}
+
+	discord.AddHandler(onMessageCreate)
+
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+
+	err = discord.Open()
+	if err != nil {
+		OjosamaErrorHandling("コネクションエラーです！", err)
+		return
+	}
+
+	if runningText, err := ojosama.Convert("ボット実行中です！", nil); err == nil {
+		fmt.Println(runningText)
+	} else {
+		panic(err)
+	}
+
+
+	sc := make(chan os.Signal, 1)
+
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
+}
